8d04569c70a0789d6650cb21144a38ac5faf15c0
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2022-04-25 17:20:55 +0900

    project

--- JS/CallbackFunction/main.js
+++ JS/CallbackFunction/main.js
@@ -39,3 +39,13 @@ console.log(calculator(sub, 7, 3));
 console.log(calculator(mul, 7, 3));
 console.log(calculator(div, 7, 3));
 console.log(calculator(test, 7, 3));
+
+
+// call by value
+// 값에 의한 복사로 함수 내에서 매개 변수 값을 변경 시켜도 영향이 미치지 않음
+// 원시 타입(primitive type)을 매개 변수로 넘겼을때 발생
+
+// call by reference
+// 주소에 대한 복사로 함수 내에서 매개 변수 내 값을 변경시키면 원본 데이터에도 영향
+// 객체 타입(object type)을 매개 변수로 넘겼을 때 발생
+
