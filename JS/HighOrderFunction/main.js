f703030aef0bf58d5c0dd5a0261b405e80552c8b
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2022-05-03 22:45:38 +0900

    JS_HighOrderFunction

--- JS/HighOrderFunction/main.js
+++ JS/HighOrderFunction/main.js
@@ -74,3 +74,86 @@ console.log(nums.sort(descendingOrder));
 
 console.log(fruits.sort(descendingOrder));
 console.log(fruits.sort(asscendingOrder));
+
+
+// forEach()
+// 배열 요소 별 콜백 함수 각각에 실행 Array..forEach(function(item,index,array){})
+// item: 배열 요소, index: 배열 위치, array: 배열
+
+// use for loop
+nums = [1,2,3];
+for(let i = 0; i< nums.length; i++)
+{
+    console.log(nums[i]);
+}
+
+nums.forEach(function(i){ 
+    console.log(i);
+})
+
+// map
+// 배열 요소 별 호출 및 결과를 배열로 반환: Array.map(function(item,index,array){})
+
+nums = [1,2,3,4,5];
+let use_for_loop = [];
+
+for(let i = 0; i< nums.length; i++)
+{
+    use_for_loop.push(nums[i]*2);
+}
+console.log(use_for_loop);
+
+let use_map = nums.map(function (item){
+    return item * 2;
+});
+
+
+//find 
+// 콜백 함수의 ㅗㅈ건을 만족하는, 단 하나의 값만 반환: Array.find(function(item,index,array){})
+
+let users = [
+    {name: 'bob', age: 17, job: false},
+    {name: 'alice', age: 20, job: false},
+    {name: 'john', age: 27, job: true},
+]
+
+let find_job = users.find(function(user){
+    return user.job == false;
+});
+
+console.log(find_job);
+
+let find_age = users.find(function(user){
+    return user.age >= 19;
+});
+console.log(find_age);
+
+//filter
+// find와 달리 모두 반환
+
+let filter_job = users.filter(function(user){
+    return user.job == false;
+});
+
+console.log(filter_job);
+
+let filter_age = users.filter(function(user){
+    return user.age >= 19;
+});
+console.log(filter_age);
+
+
+// reduce()
+// 요소 별 함수 수행 누적 결과값 반환: Array.reduce(function(accumulator, item, index, array){},initial)
+// accumulator: 이전 함수 결과 (initial로 초기값 설정 가능), item: 배열 요소, index: 배열 위치, array: 배열
+nums = [1,2,3,4,5];
+let call_count = 0;
+
+console.log("result\tvalue\tindex\t");
+let sum = nums.reduce(function (accumulator, item, index, array){
+    console.log(accumulator,"\t\t",item,"\t\t",index);
+    call_count++;
+    return accumulator+item;
+},0);
+console.log(call_count);
+console.log(sum);
